6d68590104471bd7a40a03831f34185ce90605cb
Rafa Llorens Blanes <rllorensblanes@gmail.com>
2021-12-03 12:54:36 +0100

    Models/codes.cs

---
+++ Models/codes.cs
@@ -0,0 +1,12 @@
+namespace cervezarteamdelta.Models;
+
+public class Codes
+{
+    public int Code {get; set;}
+    public string? Email {get; set;}
+    public DateTime? Date_asignment {get; set;}
+    public string? User {get; set;}
+    public string? Name {get; set;}
+    public Boolean Check_Used {get; set;} 
+
+}
